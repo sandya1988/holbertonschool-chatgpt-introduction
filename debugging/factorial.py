e381755f3f280f7548344b9cf1452437cc5d6ecc
Sandya1988 <Aoun.sandy@gmail.com>
2024-05-15 22:27:12 +0300

    dd

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -8,5 +8,14 @@ def factorial(n):
         n = n - 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if len(sys.argv) != 2:
+    print("Usage: ./factorial.py <number>")
+    sys.exit(1)
+
+try:
+    f = factorial(int(sys.argv[1]))
+    print(f)
+except ValueError:
+    print("Error: Invalid input. Please provide a valid integer.")
+    sys.exit(1)
+    
